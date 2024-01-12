3b56b1f447306088988ffbb897ced2f63bb29216
Anna Offenwanger <dev@offenwanger.ca>
2024-01-12 19:25:35 +0100

    add highlight rotation function

--- js/highlight_ring.js
+++ js/highlight_ring.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 
 export function HighlightRing(parentScene) {
+    const UP = new THREE.Vector3(0, 1, 0);
+
     const geometry = new THREE.CylinderGeometry(0.5, 0.4, 0.3, 32, 1, true)
     const texture = new THREE.TextureLoader().load('assets/flat_halo_v1.png');
     texture.wrapS = THREE.RepeatWrapping;
@@ -29,6 +31,10 @@ export function HighlightRing(parentScene) {
         mRing.position.copy(pos);
     }
 
+    function rotateUp(upVector = UP) {
+        mRing.quaternion.setFromUnitVectors(UP, upVector)
+    }
+
     function animate(time) {
         texture.offset = new THREE.Vector2(time * 0.1, 0)
     }
@@ -36,5 +42,6 @@ export function HighlightRing(parentScene) {
     this.show = show;
     this.hide = hide;
     this.setPosition = setPosition;
+    this.rotateUp = rotateUp;
     this.animate = animate;
 }

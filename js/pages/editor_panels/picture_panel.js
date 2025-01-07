a1c2bc334211ab305b96b2612d5d2ea678d833da
Anna Offenwanger <dev@offenwanger.ca>
2025-01-07 19:52:50 +0100

    add rotations to pictures

--- js/pages/editor_panels/picture_panel.js
+++ js/pages/editor_panels/picture_panel.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import { Data } from "../../data.js";
 import { ButtonInput } from "../components/button_input.js";
 import { TextInput } from "../components/text_input.js";
@@ -70,6 +71,22 @@ export function PicturePanel(container) {
             await mUpdateAttributeCallback(mPictureId, { z: newNum });
         });
 
+    let mRotationXInput = new TextInput(mPanelContainer, 'number')
+        .setLabel("ψ")
+        .setOnChange(async () => {
+            await mUpdateAttributeCallback(mPictureId, { orientation: getOrientationArray() });
+        });
+    let mRotationYInput = new TextInput(mPanelContainer, 'number')
+        .setLabel("θ")
+        .setOnChange(async () => {
+            await mUpdateAttributeCallback(mPictureId, { orientation: getOrientationArray() });
+        });
+    let mRotationZInput = new TextInput(mPanelContainer, 'number')
+        .setLabel("φ")
+        .setOnChange(async () => {
+            await mUpdateAttributeCallback(mPictureId, { orientation: getOrientationArray() });
+        });
+
     let mScaleInput = new TextInput(mPanelContainer, 'number')
         .setLabel("Scale")
         .setOnChange(async (newNum) => {
@@ -111,6 +128,13 @@ export function PicturePanel(container) {
         mPositionZInput.setText(Math.round(mPicture.z * 1000) / 1000);
         mScaleInput.setText(Math.round(mPicture.scale * 1000) / 1000);
 
+        let quat = new THREE.Quaternion(...mPicture.orientation);
+        let euler = new THREE.Euler().setFromQuaternion(quat);
+
+        mRotationXInput.setText(Math.round(180 / Math.PI * euler.x * 1000) / 1000);
+        mRotationYInput.setText(Math.round(180 / Math.PI * euler.y * 1000) / 1000);
+        mRotationZInput.setText(Math.round(180 / Math.PI * euler.z * 1000) / 1000);
+
         let teleport = model.teleports.find(t => t.attachedId == pictureId);
         if (teleport) {
             mAttachedTeleport
@@ -133,6 +157,19 @@ export function PicturePanel(container) {
 
 
         mPanelContainer.style['display'] = '';
+    }
+
+    function getOrientationArray() {
+        let x = mRotationXInput.getText();
+        let y = mRotationYInput.getText();
+        let z = mRotationZInput.getText();
+        if (isNaN(x) || isNaN(y) || isNaN(z)) {
+            console.error('Invalid angles: ' + x + ',' + y + ',' + z);
+            return [0, 0, 0, 1];
+        }
+        let euler = new THREE.Euler(x * Math.PI / 180, y * Math.PI / 180, z * Math.PI / 180)
+        let quat = new THREE.Quaternion().setFromEuler(euler);
+        return quat.toArray();
     }
 
     function hide() {
